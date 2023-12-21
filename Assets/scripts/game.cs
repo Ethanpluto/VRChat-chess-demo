ccb9488e77387ae038fe5687fc20d472a73891e4
Ethanpluto <47480557+Ethanpluto@users.noreply.github.com>
2023-12-21 17:28:06 -0500

    test

---
+++ Assets/scripts/game.cs
@@ -0,0 +1,16 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class game : UdonSharpBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public Vector3 toLocalPos(Vector3 pos){return new Vector3(-5.25f+1.5f*pos.x,pos.y,5.25f-1.5f*pos.z);}
+    public Vector3 toBoardPos(Vector3 pos){return new Vector3((5.25f+pos.x)/1.5f,pos.y,(5.25f-pos.z)/1.5f);}
+}
---
+++ Assets/scripts/test.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class test : UdonSharpBehaviour
+{
+    public Vector3 snapPos;
+    public Quaternion snapRot;
+    void Start()
+    {
+        snapPos = gameObject.transform.position;
+        snapRot = gameObject.transform.rotation;
+    }
+
+    void OnDrop()
+    {
+        gameObject.transform.position = snapPos;
+        gameObject.transform.rotation = snapRot;
+    }
+}
+ 
